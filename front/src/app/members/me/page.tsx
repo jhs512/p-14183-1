0e5e5304406d3a5865575bae94f3aa300fe041c6
jhs512 <jangka512@gmail.com>
2026-07-08 11:42:28 +0900

    069-2

--- front/src/app/members/me/page.tsx
+++ front/src/app/members/me/page.tsx
@@ -3,13 +3,18 @@
 import { useAuthContext } from "@/global/auth/hooks/useAuth";
 
 export default function Page() {
-  const { loginMember } = useAuthContext();
+  const { isLogin, loginMember } = useAuthContext();
+
+  if (!isLogin) return <div>로그인 후 이용해주세요.</div>;
 
   return (
     <>
       <h1>내 정보</h1>
 
-      <div>{JSON.stringify(loginMember)}</div>
+      <div>ID : {loginMember.id}</div>
+      <div>가입 : {loginMember.createDate}</div>
+      <div>수정 : {loginMember.modifyDate}</div>
+      <div>이름 : {loginMember.name}</div>
     </>
   );
 }
