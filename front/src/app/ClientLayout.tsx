277fcfca022bb351d96d46e0ac777b709ab9fd3f
jhs512 <jangka512@gmail.com>
2026-07-08 11:23:49 +0900

    054

--- front/src/app/ClientLayout.tsx
+++ front/src/app/ClientLayout.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import client from "@/global/backend/client";
 
 export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+  const logout = () => {
+    client.DELETE("/api/v1/members/logout").then((res) => {
+      if (res.error) {
+        alert(res.error.msg);
+        return;
+      }
+
+      router.replace("/");
+    });
+  };
+
   return (
     <>
       <header>
@@ -17,6 +33,12 @@ export default function ClientLayout({
           <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
             글 목록
           </Link>
+          <Link href="/members/login" className="p-2 rounded hover:bg-gray-100">
+            로그인
+          </Link>
+          <button onClick={logout} className="p-2 rounded hover:bg-gray-100">
+            로그아웃
+          </button>
         </nav>
       </header>
       <main className="flex-1 flex flex-col">{children}</main>
